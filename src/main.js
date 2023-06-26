548fbd21010ad5d6122faf52af813cf28f8d35f8
DannielNavas <dannielnavas@gmail.com>
2023-06-26 07:15:23 -0500

    ✨ init config iconify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { Icon } from "@iconify/vue";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
+app.component("icon", Icon);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
