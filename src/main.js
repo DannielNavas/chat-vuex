edd399d92719f5206e9a2ef9a1b48684ff8ebacc
DannielNavas <dannielnavas@gmail.com>
2023-06-26 07:33:27 -0500

    ✨ initial project for vuex implementation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import "./assets/main.css";
+import "./index.css";
 
 import { Icon } from "@iconify/vue";
 import { createApp } from "vue";
@@ -6,8 +6,8 @@ import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
-app.component("icon", Icon);
 
 app.use(router);
+app.component("Icon", Icon);
 
 app.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
+      children: [
+        {
+          path: ":id(\\d+)",
+          component: () => import("../views/MessagesView.vue"),
+        },
+        {
+          path: ":(.*)",
+          component: () => import("../views/VoidMessagesView.vue"),
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/profile",
+      name: "profile",
+      component: () => import("../views/ProfileView.vue"),
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
